6d4073cf386a1859c289d0b3aa2a846c3da7f793
dennis bergevin <65262795+drb198@users.noreply.github.com>
2020-06-23 20:33:42 -0400

    Fixed baseURL

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 const key = '31f7cae34b212755c6eb67c4ae9679e2';
 
 const requestCity = async (city) => {
-    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
